5160a8e8259d70eb79be79da27c7469982d04cb3
JoaoDiaas12 <jvitor.1212@hotmail.com>
2024-12-16 20:03:43 -0300

    Desafio do Felipão

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-let laranja = "uva"
-
-console.log ("a melhor fruta do mundo é " + laranja)
+let NomeHeroi = "Jovi Cavaleiro"
+let nivelXP = 150000
+if(nivelXP < 1000){
+console.log("O Herói de nome " + NomeHeroi + " está no nível de Ferro!" )
+}else if(nivelXP >= 1001 && nivelXP <= 2000){
+console.log("O Herói de nome " + NomeHeroi + " está no nível de Bronze!")
+}else if(nivelXP >= 2001 && nivelXP <= 5000){
+console.log("O Herói de nome " + NomeHeroi + " está no nível de Prata!")
+}else if(nivelXP >= 5001 && nivelXP <= 7000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Ouro!")
+}else if(nivelXP >= 7001 && nivelXP <= 8000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Platina!")
+}else if(nivelXP >= 8001 && nivelXP <= 9000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Ascendente!")
+}else if(nivelXP >= 9001 && nivelXP <= 10000){
+    console.log("O Herói de nome " + NomeHeroi + " está no nível de Imortal!")
+}else if(nivelXP >= 10001){
+console.log("O Herói de nome " + NomeHeroi + " está no nível de Radiante!")}
